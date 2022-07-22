3f4f2652cf533c8aba9cabdb36283f78fd221bfe
Rosario Cordero <rosa.corderoe@gmail.com>
2022-07-22 18:57:35 -0400

    destacados

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,3 +3,13 @@
 document.getElementById ("correoEnviado").addEventListener("click", function(){
     alert("Correo Enviado");
 })
+
+
+/*SECCIÓN DESTACADOS _ Card toggle_ Aplicado a los títulos de las cards*/
+
+$('.card-title').on('click', function() {
+    $('.card-text').toggle('fast', function() {
+    });
+});
+
+
